90cfdffe06064dfa808176991d4a2c2ae6b24d39
zzxskfd <10861522+zzxskfd@users.noreply.github.com>
2026-06-11 13:06:16 +0800

    Add logo and description

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/GooseLegChallenge/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
